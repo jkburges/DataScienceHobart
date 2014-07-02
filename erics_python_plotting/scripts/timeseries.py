a0b7e79d3fbbae8c60c883c0873519486dba9428
Jon Burgess <jkburges@gmail.com>
2014-07-03 09:21:40 +1000

    Saving all images.

--- erics_python_plotting/scripts/timeseries.py
+++ erics_python_plotting/scripts/timeseries.py
@@ -12,7 +12,7 @@ import mpl_toolkits.basemap as bm
 #
 
 # Load data
-data = np.load('~/erics_python_plotting/data/temp_ts.npz')
+data = np.load('../data/temp_ts.npz')
 time = data['time']
 lon = data['lon']
 lat = data['lat']
@@ -27,7 +27,7 @@ plt.plot(time, T_A1B[:,1], 'r-')
 plt.ylabel(r'Temperature [$^\circ$C]')
 plt.xlabel('time [years]')
 plt.grid()
-plt.savefig('temp_ts.png')
+plt.savefig('temp_ts_single.png')
 
 plt.figure() # Four time series, as a 4x1 subplot matrix
 for i in range(len(lon)):
@@ -43,6 +43,8 @@ for i in range(len(lon)):
     else:
         plt.xlabel('time [years]')
 
+plt.savefig('temp_ts_four.png')
+
 #
 # Histogram and probability density function
 #
@@ -52,6 +54,8 @@ plt.figure()
 plt.hist(T_CTRL[:,1], 30)
 plt.ylabel('Count')
 plt.xlabel(r'Temperature [$^\circ$C]')
+
+plt.savefig('histogram.png')
 
 # Calculate pdf using a kernel density function
 from scipy import stats # need this extra module
@@ -72,6 +76,8 @@ plt.plot(T, T_A1B_pdf, 'r-')
 # Add a legend
 plt.legend(('CTRL run', 'A1B run'))
 
+plt.savefig('pdf_1.png')
+
 # Calculate pdf using a kernel density function
 plt.figure()
 sec = np.arange(np.percentile(T_CTRL[:,1], 2.5), np.percentile(T_CTRL[:,1], 97.5), 0.01)
@@ -83,4 +89,4 @@ plt.ylim(0,T_CTRL_pdf.max()*1.1)
 plt.ylabel('Probability')
 plt.xlabel(r'Temperature [$^\circ$C]')
 
-
+plt.savefig('pdf_2.png')
